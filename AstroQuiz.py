f8b0c2a5d0790df32acbe8849228db4f3112782b
AbelBahru <122306346+AbelBahru@users.noreply.github.com>
2023-04-28 19:22:08 -0400

    Update AstroQuiz.py
    figured out how the quiz will be structured, working on a scoring system

--- AstroQuiz.py
+++ AstroQuiz.py
@@ -1,8 +1,14 @@
 print("Welcome to AstroQuiz!")
 
-playing = input("Do you want to play? ")
+playing = input("Do you want to play? ").lower()
 
 if playing != "yes":
     quit()
 
 print("Okay then! lets takeoff.")
+
+answer = input("Which planet is closest to the sun? ").lower()
+if answer == "mercury":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
