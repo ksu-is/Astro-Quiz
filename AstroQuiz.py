1a247f9c7d6624b186f627cabec84c980566cfe3
AbelBahru <122306346+AbelBahru@users.noreply.github.com>
2023-04-28 19:57:35 -0400

    Update AstroQuiz.py
    added 20 questions about each of the planets + the moon + ISS

--- AstroQuiz.py
+++ AstroQuiz.py
@@ -7,8 +7,122 @@ if playing != "yes":
 
 print("Okay then! lets takeoff.")
 
-answer = input("Which planet is closest to the sun? ").lower()
+answer = input("I am the closest planet to the sun. Who am I? ").lower()
 if answer == "mercury":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("My nickname is the red planet. What is my real name? ").lower()
+if answer == "mars":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I have a thick atmosphere that is hot enough to melt lead, what am I called? ").lower()
+if answer == "venus":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I am the largest planet in our solar system. Who am I? ").lower()
+if answer == "jupiter":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I am smaller than Earth and my surface is covered with craters. Who am I? ").lower()
+if answer == "mercury":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I am sometimes called the morning star or evening star. Who am I? ").lower()
+if answer == "venus":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I am home to the only known intelligent life in the universe, do you know me? ").lower()
+if answer == "earth":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I am a potential destination for future human colonization. Who am I? ").lower()
+if answer == "mars":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I have four major moons named Io, Europa, Ganymede, and Callisto. Who am I? ").lower()
+if answer == "jupiter":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("My beautiful rings are one of my most famous features. Who am I? ").lower()
+if answer == "saturn":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I have a surface that's about 71 percent covered by water. Who am I? ").lower()
+if answer == "earth":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("My density is less than water, so I could float in it. Guess who? ").lower()
+if answer == "saturn":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I rotate sideways and I am classified as an ice giant. What planet am i? ").lower()
+if answer == "uranus":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I am the furthest planet from the Sun and appear blue. Whats my name? ").lower()
+if answer == "neptune":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I was once considered the ninth planet, but now I am classified as a dwarf planet. What do they call me? ").lower()
+if answer == "pluto":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I am a space laboratory orbiting Earth, where astronauts from different countries conduct research. Whats my name? ").lower()
+if answer == "international space station" or answer == "iss":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I have a blue-green color due to the presence of methane in my atmosphere. Who am I? ").lower()
+if answer == "uranus":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I have the largest volcano in the solar system, named Olympus Mons. Guess who am I? ").lower()
+if answer == "mars":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+
+answer = input("I am now the smallest planet in the solar system after pluto got kicked out of the club. Who am I? ").lower()
+if answer == "mercury":
+    print("Correct!")
+else:
+    print("Incorrect :(" )
+    
+answer = input("Astronauts landed on me in the summer of 1969. Whats my name? ").lower()
+if answer == "moon" or answer == "the moon":
     print("Correct!")
 else:
     print("Incorrect :(" )
